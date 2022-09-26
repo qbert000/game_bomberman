a2ea7eee2dd0eb8a6c92f8c18cba322514235563
qbert000 <quyen1412kid@gmail.com>
2022-09-26 16:35:52 +0700

    Initial commit

---
+++ src/main/java/Player.java
@@ -0,0 +1,10 @@
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.pathfinding.CellMoveComponent;
+
+import java.awt.*;
+
+public class Player extends Component {
+
+    private CellMoveComponent cell;
+
+}
