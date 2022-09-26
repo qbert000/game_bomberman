ea2de0933ae1c6610429bc18a51e80381f6f4195
qbert000 <100498786+qbert000@users.noreply.github.com>
2022-09-26 16:59:23 +0700

    Add files via upload

--- src/main/java/Factory.java
+++ src/main/java/Factory.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Factory {
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.EntityFactory;
+import com.almasb.fxgl.entity.SpawnData;
+import com.almasb.fxgl.entity.Spawns;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Factory implements EntityFactory {
+
+    @Spawns("wall")
+    public Entity newWall(SpawnData data) {
+        return FXGL.entityBuilder(data)
+                .viewWithBBox(new Rectangle(30,30, Color.PINK))
+                .build();
+    }
 }
